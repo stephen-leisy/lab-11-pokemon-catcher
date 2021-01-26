0ca64bae29a550f4bad9c5cf9c32fa02998d8c51
stephen-leisy <stleisy@gmail.com>
2021-01-25 17:58:37 -0800

    forgot to commit but added local storage elements, seen and caught counters, and game play clock.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import functions and grab DOM elements
-import { getRandomPokemon } from './app-utils.js';
+import { getRandomPokemon, findPokemonByID } from './app-utils.js';
 import pokemon from './data.js';
+
 // initialize state
 
 // set event listeners to update state and DOM
@@ -14,9 +15,12 @@ const POKESTATS = 'POKESTATS';
 //     seen: 5
 // }
 // ];
-
+let gameCounter = 0;
 
 export function setThreePokemon() {
+
+    gameCounter++;
+
     let pokeOne = getRandomPokemon();
     let pokeTwo = getRandomPokemon();
     let pokeThree = getRandomPokemon();
@@ -32,6 +36,15 @@ export function setThreePokemon() {
     const img2 = matchPokeImage(pokeTwo);
     const img3 = matchPokeImage(pokeThree);
 
+    incrementEncounterCounter(pokeOne.id);
+    incrementEncounterCounter(pokeTwo.id);
+    incrementEncounterCounter(pokeThree.id);
+
+
+
+
+
+
 
     const div = document.getElementById('pokemon-window');
     div.textContent = '';
@@ -42,9 +55,70 @@ export function setThreePokemon() {
 setThreePokemon();
 
 
+
+
+
 function matchPokeImage(boogerPokemon) {
     const pokemonImage = document.createElement('img');
     pokemonImage.src = boogerPokemon.url_image;
+    pokemonImage.classList.add('poke-class');
+
+    pokemonImage.addEventListener('click', () => {
+        caughtCounter(boogerPokemon.id);
+
+        if (gameCounter < 10) {
+            setThreePokemon();
+        } else {
+            window.location = 'results';
+        }
+    });
 
     return pokemonImage;
 }
+
+export function getPokeStats() {
+    let stats = JSON.parse(localStorage.getItem(POKESTATS));
+    if (!stats) {
+        stats = [];
+        const stringyPokeStats = JSON.stringify(POKESTATS);
+        localStorage.setItem([], stringyPokeStats);
+    }
+    return stats;
+
+}
+
+export function setPokeStats(newStats) {
+    let newStringStats = JSON.stringify(newStats);
+    localStorage.setItem(POKESTATS, newStringStats);
+}
+
+export function incrementEncounterCounter(id) {
+    const stats = getPokeStats(id);
+
+    const specificPoke = findPokemonByID(stats, id);
+
+    if (!specificPoke) {
+        const newPokeStat = {
+            id: id,
+            seen: 1,
+            caught: 0,
+        };
+        stats.push(newPokeStat);
+    } else {
+        specificPoke.seen++;
+    }
+    setPokeStats(stats);
+
+}
+
+export function caughtCounter(id) {
+    const stats = getPokeStats();
+
+    const specificPoke = findPokemonByID(stats, id);
+    specificPoke.caught++;
+    setPokeStats(stats);
+}
+
+
+
+
---
+++ local-storage-utils.js
@@ -0,0 +1,11 @@
+
+const POKESTATS = 'POKESTATS';
+
+// const pokeStats = [{
+//     id: 1;
+//     name: Pikachu;
+//     caught: 2;
+//     seen: 5
+// }
+// ];
+
